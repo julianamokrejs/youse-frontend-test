5c9671be49462a28a9eb9ea59814aad5eeb35000
julianamokrejs <ju.mokrejs@gmail.com>
2020-04-08 18:04:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import faker from "faker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css"
+import { Container } from "react-bootstrap";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
+  renderTable() {
+    return (
+      <BootstrapTable search={ true } searchPlaceholder='Buscar' pagination data={this.state.users} >
+        <TableHeaderColumn dataField="name" isKey >Nome</TableHeaderColumn>
+        <TableHeaderColumn dataField="email">Email</TableHeaderColumn>
+        <TableHeaderColumn dataField="city">Cidade</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+
+  componentDidMount() {
+    for (let i = 0; i < 100; i++) {
+      const user = {
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        city: faker.address.city()
+      };
+      this.setState(prevState => ({
+        users: [...prevState.users, user]
+      }));
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <div className="wrapper">
+          {this.renderTable()}
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default App;
